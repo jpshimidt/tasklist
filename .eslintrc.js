3e6229620b0bee8c11b7d43b9008bcc17da1be0a
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2023-11-05 12:38:41 -0300

    config: eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassing": "off",
+    camelcase: "off",
+    "no-underscore_dangle": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+  },
+};
