864a98f95c8d854613a7afcaab87b0f63bfa7b4b
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2023-11-05 13:51:14 -0300

    database: Add migrations

---
+++ src/app/models/User.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Sequelize, { Model } from "sequelize";
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      },
+    );
+  }
+}
+
+export default User;
