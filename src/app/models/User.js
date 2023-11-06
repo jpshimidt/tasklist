a7b7ec33867e13dbded4de0d72ca31889f04920e
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2023-11-06 00:45:33 -0300

    chore: Add crypto in password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-param-reassign */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Sequelize, { Model } from "sequelize";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import bcrypt from "bcrypt";
 
 class User extends Model {
   static init(sequelize) {
@@ -7,12 +10,21 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
         sequelize,
       },
     );
+
+    this.addHook("beforeSave", async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 
