d3f854603a946d596fed2c5354209b2982ba161a
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2023-11-12 02:19:33 -0300

    Add lib CORS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 
@@ -11,6 +13,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
